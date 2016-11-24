4ef094f46d00f8dba780b5e1eb4cf5403cc396bf
paco39 <pako03.fm@gmail.com>
2016-11-24 00:44:21 -0600

    Se crearon algunas pantallas correspondientes a los casos de uso identificados.

--- CafeteriaEscolar/src/cafeteriaescolar/CafeteriaEscolar.java
+++ CafeteriaEscolar/src/cafeteriaescolar/CafeteriaEscolar.java
@@ -5,6 +5,8 @@
  */
 package cafeteriaescolar;
 
+import cafeteriaGUI.AdministrarProductos;
+
 /**
  *
  * @author Paco Miranda
@@ -16,6 +18,9 @@ public class CafeteriaEscolar {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        AdministrarProductos aP=new AdministrarProductos();
+        aP.setVisible(true);
+        
     }
     
 }
